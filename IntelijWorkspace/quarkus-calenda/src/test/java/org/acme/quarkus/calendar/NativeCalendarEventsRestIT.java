fb32db0c6fc4028c4e2eea916cd3fd0e571cf88e
Mateusz Szczech <4561235>
2021-10-31 19:00:14 +0100

    Deleted old files

--- IntelijWorkspace/quarkus-calenda/src/test/java/org/acme/quarkus/calendar/NativeCalendarEventsRestIT.java
+++
@@ -1,9 +0,0 @@
-package org.acme.quarkus.calendar;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCalendarEventsRestIT extends CalendarEventsRestTest {
-
-    // Execute the same tests but in native mode.
-}
