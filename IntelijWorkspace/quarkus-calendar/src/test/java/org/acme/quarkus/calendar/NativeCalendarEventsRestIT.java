4ad49e03bf7040c6e303f9bd9ccd7679fd3b0fb2
Szczech <4561235>
2021-10-31 14:10:38 +0100

    QUARKUS project creation

---
+++ IntelijWorkspace/quarkus-calendar/src/test/java/org/acme/quarkus/calendar/NativeCalendarEventsRestIT.java
@@ -0,0 +1,9 @@
+package org.acme.quarkus.calendar;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCalendarEventsRestIT extends CalendarEventsRestTest {
+
+    // Execute the same tests but in native mode.
+}
