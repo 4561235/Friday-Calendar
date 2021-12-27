62aad73636413819c2aadd88504e6136681c607b
luca <luca.gasche@gmail.com>
2021-12-27 02:51:45 -0800

    Refactor Event.

--- friday-back/src/main/java/uge/friday/rest/Event.java
+++ friday-back/src/main/java/uge/friday/rest/Event.java
@@ -2,6 +2,7 @@ package uge.friday.rest;
 
 import javax.persistence.Entity;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import uge.friday.data.CalendarDate;
 
 /**
  * Database data persistence class.
@@ -9,8 +10,15 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
  */
 @Entity
 public class Event extends PanacheEntity {
+    private enum CalendarType {FRIDAY, GOOGLE}
+
     public String title;
-    //public String description;
+    public String location;
+    public String description;
+    public boolean recurrence;
+    public CalendarType calendarType;
+    public CalendarDate start;
+    public CalendarDate end;
 
     @Override
     public String toString() {
